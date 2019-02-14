010dc21515442016641e48c19bd76a782753cd40
jainaarohi <jainaa@wgc.school.nz>
2019-02-14 15:14:49 +1300

    added more label

--- hello_world.py
+++ hello_world.py
@@ -8,4 +8,12 @@ my_label = tkinter.Label(root)
 my_label.config(text="Hello!", fg="green")
 my_label.grid()
 
+my_label2 = tkinter.Label(root)
+my_label2.config(text="Hello there!", fg="blue")
+my_label2.grid()
+
+my_label3 = tkinter.Label(root)
+my_label3.config(text="Hello you!", fg="pink")
+my_label3.grid()
+
 root.mainloop()
