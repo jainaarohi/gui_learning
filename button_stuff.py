caba9b507ccafe2150293439c6f2ffc5175afc8c
jainaarohi <jainaa@wgc.school.nz>
2019-02-15 10:10:30 +1300

    initial commit

---
+++ button_stuff.py
@@ -0,0 +1,13 @@
+import tkinter
+root = tkinter.Tk()
+
+
+def print_hi():
+    print("hi")
+
+
+button1 = tkinter.Button(root)
+button1.config(text="morning tea soon", bg="green", fg="hot pink", font=("Papyrus", "50"), command = print_hi)
+button1.grid()
+
+root.mainloop()
